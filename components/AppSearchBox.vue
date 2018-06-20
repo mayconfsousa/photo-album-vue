cda06519ac90ec82c96341e73e49bbdbd4d58100
Maycon Sousa <mayconfsousa@gmail.com>
2018-06-19 22:34:22 -0300

    Removing v-focus directive

--- components/AppSearchBox.vue
+++ components/AppSearchBox.vue
@@ -2,7 +2,7 @@
   <transition name="slide-fade">
     <div class="form-group d-flex justify-content-between" v-if="$store.state.searchMode">
       <div class="input-group">
-        <input type="text" class="form-control" placeholder="Search" v-focus="$store.state.searchMode" />
+        <input type="text" class="form-control" placeholder="Search" />
         <div class="input-group-append">
           <button class="btn btn-danger" @click="$store.commit('toggleSearchBoxMode')">Cancel</button>
         </div>
